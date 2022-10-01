8184da080371981947aaca45f28f6fcd3d6299c2
Gogs <gogs@fake.local>
2022-10-01 21:48:03 +0800

    创建好了架子和markdown与html互相转换的小组件

--- README.md
+++
@@ -1,5 +0,0 @@
-# AbsNotebook
-This is a Markdown Notebook created by native js and some plugins(jstree、bootstrap...).
-After finished this work, I will update this project by Vue.
-# How to run this?
-
--- main.js
+++ main.js
@@ -1,117 +1,96 @@
-// import { readFiles } from './js/read-files.js'
-import fs from "fs"
+import './style.css'
+import markdownIt from 'markdown-it'
+import TurndownService from 'turndown'
 
-/**
- * 点击打开文件夹
- */
-let open_folder = document.querySelector('.open-folder')
-open_folder.onclick = function() {
-    alert("点击了")
-    let folderPath = "F:\\test01"
-    let folder = []
-    readFiles(folderPath, folder)
-    console.log(folder);
-}
+//markdown转html
+const md = new markdownIt();
+//html转markdown
+const turndownService = new TurndownService();
+turndownService.keep(['del', 'ins'])
 
+var time = 200;
+var timeOut = null;
 
-//读取文件的函数
-function readFiles(folderPath, insertFile) {
-    let regex = /\.{1}/
-    let arr = fs.readdirSync(folderPath);
-    if (arr.length == 0) return
-
-    arr.forEach(filePath => {
-        if (!regex.test(filePath)) {
-            let newPath = folderPath + "\\" + filePath
-            insertFile.push({
-                'text': filePath,
-                'children': []
-            })
-            readFiles(newPath, insertFile[insertFile.length - 1].children)
-        } else {
-            insertFile.push({
-                'text': filePath
-            })
+//设置textarea随文字内容自动调节高度
+function makeExpandingArea(el) {
+    var timer = null;
+    //由于ie8有溢出堆栈问题，故调整了这里
+    var setStyle = function(el, auto) {
+        if (auto) el.style.height = 'auto';
+        el.style.height = el.scrollHeight + 'px';
+    }
+    var delayedResize = function(el) {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
         }
-    })
-}
+        timer = setTimeout(function() {
+            setStyle(el)
+        }, 200);
+    }
+    if (el.addEventListener) {
+        el.addEventListener('input', function() {
+            setStyle(el, 1);
+        }, false);
+        setStyle(el)
+    } else if (el.attachEvent) {
+        el.attachEvent('onpropertychange', function() {
+            setStyle(el)
+        })
+        setStyle(el)
+    }
+    if (window.VBArray && window.addEventListener) { //IE9
+        el.attachEvent("onkeydown", function() {
+            var key = window.event.keyCode;
+            if (key == 8 || key == 46) delayedResize(el);
 
-//按钮点击显示和隐藏左侧侧边栏
-let close_left_bar = document.querySelector('.close-left-bar')
-let row = document.querySelector('.row')
-close_left_bar.onclick = function() {
-    if (row.style.display === 'none') {
-        row.style.display = 'block'
-    } else {
-        row.style.display = 'none'
+        });
+        el.attachEvent("oncut", function() {
+            delayedResize(el);
+        }); //处理粘贴
     }
 }
 
-// jstree的视图
-let jsTree = document.getElementById('jstree1')
-$(jsTree).jstree({
-    'core': {
-        'check_callback': true
-    },
-    'plugins': ['types', 'dnd'],
-    'types': {
-        'default': {
-            'icon': 'fa fa-folder'
-        },
-        'html': {
-            'icon': 'fa fa-file-code-o'
-        },
-        'svg': {
-            'icon': 'fa fa-file-picture-o'
-        },
-        'css': {
-            'icon': 'fa fa-file-code-o'
-        },
-        'img': {
-            'icon': 'fa fa-file-image-o'
-        },
-        'js': {
-            'icon': 'fa fa-file-text-o'
-        }
+//单击cell选中cell
+document.querySelector('.showMarkdown').addEventListener('click', function() {
+    this.stopPropagation();
+    clearTimeout(timeOut); // 清除第一个单击事件
+    timeOut = setTimeout(function() {
+        alert("单击")
+            // 单击事件的代码执行区域
+            // ...
+    }, time)
+})
 
+//双击cell进入编辑模式
+document.querySelector('.showMarkdown').addEventListener('dblclick', function() {
+    let titleEditing = false;
+    this.style.padding = "0";
+    const that = this
+    clearTimeout(timeOut); // 清除第二个单击事件
+    // alert("双击");
+    //双击的处理逻辑
+    if (titleEditing) {
+        return
     }
-});
+    titleEditing = true
+    let old = this.innerHTML
+    old = turndownService.turndown(old)
+    this.innerHTML = ""
 
-//jsTree的文件夹json数据（本来使用readFiles函数读取返回，但是浏览器端无法使用fs模块）
-let filesName = [{
-        'text': 'html',
-        'children': [{
-            'text': 'tom.js',
-            'icon': 'fa fa-file-text-o'
-        }]
-    }, {
-        'text': 'javascript',
-        'children': [{
-            'text': 'css',
-            'children': [{
-                'text': 'animate.css',
-                'icon': 'fa fa-file-text-o'
-            }],
-            'state': {
-                'opened': true
-            }
-        }, {
-            'text': 'js',
-            'children': [{
-                'text': 'bootstrap.js',
-                'icon': 'fa fa-file-text-o'
-            }, {
-                'text': 'hplus.min.js',
-                'icon': 'fa fa-file-text-o'
-            }],
-        }]
-    },
-    'css'
-]
+    let newObj = document.createElement("TEXTAREA");
+    newObj.id = "textarea"
+    newObj.className = "test_box"
+    newObj.rows = "5"
+    newObj.value = old
+    makeExpandingArea(newObj);
 
-let jsonTree = document.querySelector('#using_json')
-$(jsonTree).jstree({
-    'core': {
-        'data': filesName
+    this.append(newObj)
+    newObj.focus()
+    newObj.onblur = function() {
+        that.innerHTML = md.render(this.value)
+        titleEditing = false
+        that.style.padding = "10px 10px";
+        makeExpandingArea(newObj);
     }
-});
+})
